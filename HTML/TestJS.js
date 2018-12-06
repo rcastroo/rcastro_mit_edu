1e77f96f936de1cd3c3d1f9d0fc64eec057a5bc9
rcastro <rcastro@mit.edu>
2018-12-06 01:22:45 -0500

    blogsss

--- HTML/TestJS.js
+++ HTML/TestJS.js
@@ -1,4 +1,8 @@
 
+function testnow(){
+    document.getElementById("DEBUG_1").innerText = "GOT HERE 0";
+}
+
 let isMobile = false; //initiate as false
 if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
@@ -8,7 +12,6 @@ if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elain
 
 let currentPIs = null;
 let pageContainer  = document.createElement('div');;
-let HBoxContainer = document.createElement('div');
 let selection = "All";
 let loadedOnce=false;
 
@@ -16,10 +19,16 @@ let regeneration = document.createEvent('Event');
 regeneration.initEvent('regeneration', true, true);
 
 
+function dothis() {
+    document.getElementById("first").innerHTML = "YAP";
+}
+
+dothis();
+
+
 // importance(1-10max)
 function PortfolioItem(title, summary, date, type, importance, picture, url, buildBlog_redirect, tags, inProgress) {
     // document.getElementById("test_container").innerText = "GOT HERE-0.6";
-    console.log(title);
     this.title = title;
     this.summary = summary;
     this.date = date;
@@ -63,16 +72,12 @@ function generatePage(portfolioItems) {
         if(selection=="All"){
             return true;
         }
-        PI.tags.split(';').forEach(function(tag){
-            isSelected= tag==selection? true: isSelected;
-        });
+        PI.tags.split(';').forEach(tag=> isSelected= tag==selection? true: isSelected);
         return isSelected;
     }).sort(function (a, b) {
         return b.importance - a.importance;
     });
-    sorted.forEach(function(PI){
-        PIsover7 = PI.importance>=7? PIsover7+1:PIsover7;
-    });
+    sorted.forEach((PI)=> PIsover7 = PI.importance>=7? PIsover7+1:PIsover7);
     PIsover7 = PIsover7==0? 1:PIsover7;
     let count = 0;
 
@@ -112,13 +117,17 @@ function generatePage(portfolioItems) {
 
     //get rid of all hboxes, to put in new ones
     count = 0;
-
-
-    while (HBoxContainer.firstChild) {
-        HBoxContainer.removeChild(HBoxContainer.firstChild);
+    let timeOut=0;
+    while (pageContainer.children.item(count) && timeOut<5000) {
+        if(pageContainer.children.item(count).className.startsWith("HBox")){
+            pageContainer.removeChild(pageContainer.children.item(count));
+        }else{
+            count++;
+        }
+        timeOut++;
     }
     HBoxsARR.forEach(function (hbox) {
-        HBoxContainer.appendChild(hbox);
+        pageContainer.appendChild(hbox);
     });
 
     pageContainer.dispatchEvent(regeneration);
@@ -132,23 +141,20 @@ function test0(portfolioItems) {
 
     pageContainer.style.width = "100%";
     pageContainer.style.height ="100%";
-    HBoxContainer.style.width ="100%";
-    HBoxContainer.style.height = "100%";
 
     //create tagMap to get the most popular tags, then make the selection bar
-    let tagMap = {};
+    let tagMap = new Map();
     portfolioItems.forEach(function(PI){
         PI.tags.split(';').forEach(function(tag){
-            tagMap[tag] = tagMap.hasOwnProperty(tag)? tagMap[tag]+1 : 1 ;
+            tagMap.set(tag, tagMap.get(tag)==null? 1:tagMap.get(tag)+1 );
         });
     });
-    let arrangedTags = Object.keys(tagMap);
+    let arrangedTags = Array.from(tagMap.keys());
     arrangedTags.sort(function(a,b){
-        return tagMap[b] -tagMap[a];
+        return tagMap.get(b) -tagMap.get(a);
     });
 
-     pageContainer.appendChild(generateSelectionBar(arrangedTags));
-     pageContainer.appendChild(HBoxContainer);
+    pageContainer.appendChild(generateSelectionBar(arrangedTags));
 
     //
     currentPIs= portfolioItems;
@@ -233,14 +239,14 @@ function generateSelectionBar(type_selections) {
             bar.style.background = getColorFromType(selection);
             generatePage(currentPIs);
         };
-         type_selections.forEach(function(type){
-             let option = document.createElement("option");
-             option.text=type;
-             selector.add(option);
+        type_selections.forEach(function(type){
+            let option = document.createElement("option");
+            option.text=type;
+            selector.add(option);
 
-         });
+        });
 
-         bar.appendChild(selector);
+        bar.appendChild(selector);
     }
 
     bar.className = "selectionBar";
@@ -418,4 +424,5 @@ function generatePISkeleton(portfolioItem) {
 
     return container;
 }
+
 
